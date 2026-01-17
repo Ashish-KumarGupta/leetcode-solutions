d5950d72c709f5e35c3b6502e547592fee8145cb
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-01-17 18:03:39 +0530

    Time: 7 ms (21.57%) | Memory: 13.9 MB (64.75%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,10 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        j = 1
+        # j = 1
         
-        for i in range(len(nums)):
-            if nums[i] != nums[j-1]:
-                nums[j] = nums[i]
-                j +=1
-        return j
+        # for i in range(len(nums)):
+        #     if nums[i] != nums[j-1]:
+        #         nums[j] = nums[i]
+        #         j +=1
+        # return j
+
+        i = 0
+
+
+        for j in range(len(nums)):
+            if nums[i] < nums[j]:
+                temp = nums[i+1]
+                nums[i+1] = nums[j]
+                nums[j] = temp
+                i +=1
+        return i+1
