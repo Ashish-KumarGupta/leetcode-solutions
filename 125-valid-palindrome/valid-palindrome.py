56d13061a30eba259c4eabb10feef56c60a559b8
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-01-21 21:45:49 +0530

    Time: 20 ms (39.84%) | Memory: 12.8 MB (76.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,14 +1,31 @@
 class Solution(object):
     def isPalindrome(self, s):
-        st2=""
-        for i in s:
-            if(i>='a' and i<='z' or i>='A' and i<='Z'):
-                st2+=i.lower()
-            elif(i>='0' and i<='9'):
-                st2+=i
-        temp=st2[::-1]
-        if(temp==st2):
-            return True
-        else:
-            return False
-        
+        # st2=""
+        # for i in s:
+        #     if(i>='a' and i<='z' or i>='A' and i<='Z'):
+        #         st2+=i.lower()
+        #     elif(i>='0' and i<='9'):
+        #         st2+=i
+        # temp=st2[::-1]
+        # if(temp==st2):
+        #     return True
+        # else:
+        #     return False
+
+
+        left, right = 0, len(s)-1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
