4eba38c8f9496dc721747298c63724652f18c6ea
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-01-28 17:43:48 +0530

    Time: 4 ms (96.66%) | Memory: 13.6 MB (62.40%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -5,29 +5,29 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # map_st = {}
-        # map_ts = {}
+        map_st = {}
+        map_ts = {}
 
-        # for i in range(len(s)):
-        #     if s[i] in map_st:
-        #         if map_st[s[i]] != t[i]:
-        #             return False
-        #     else:
-        #         if t[i] in map_ts:
-        #             return False
-        #         map_st[s[i]] = t[i]
-        #         map_ts[t[i]] = s[i]
+        for i in range(len(s)):
+            if s[i] in map_st:
+                if map_st[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in map_ts:
+                    return False
+                map_st[s[i]] = t[i]
+                map_ts[t[i]] = s[i]
 
-        # print(map_st[s[i]])
+        print(map_st[s[i]])
 
-        # return True
+        return True
 
-        dic1 =[]
-        dic2 = []
+        # dic1 =[]
+        # dic2 = []
 
-        for i in s:
-            dic1.append(s.index(i))
+        # for i in s:
+        #     dic1.append(s.index(i))
 
-        for i in t:
-            dic2.append(t.index(i))
-        return dic1 == dic2
+        # for i in t:
+        #     dic2.append(t.index(i))
+        # return dic1 == dic2
