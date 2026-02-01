b9c5c3d6d075e9c49ad2b9bae9e18a4dcf0c7f44
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-02-01 11:22:37 +0530

    Time: 4 ms (45.45%) | Memory: 12.5 MB (54.55%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -4,6 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums[1:] = sorted(nums[1:])
-        return (nums[0] + nums[1] + nums[2])
+        # nums[1:] = sorted(nums[1:])
+        # return (nums[0] + nums[1] + nums[2])
+
+
+        min1 = float('inf')
+        min2 = float('inf')
+
+        for i in nums[1:]:
+            if i < min1:
+                min2 = min1
+                min1 = i
+            elif i < min2:
+                min2 = i
+
+        return nums[0] + min1 + min2
+
         
+
+
