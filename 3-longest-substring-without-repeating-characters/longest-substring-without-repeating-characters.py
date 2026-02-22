a0789c97e7fb5727530189d813a4fa534c7d6113
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-02-22 08:30:51 +0530

    Time: 16 ms (86.32%) | Memory: 12.8 MB (69.87%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -13,3 +13,23 @@ class Solution(object):
             mapp[s[r]] = r
             r +=1
         return mxlen
+
+    
+
+        # mapp = {} 
+        # l = 0
+        # r = 0
+        # n = len(s)
+        # maxLen = 0
+
+        # while r < n:
+
+        #     if s[r] in mapp and mapp[s[r]] >= l:
+        #         l = mapp[s[r]] + 1
+
+        #     mapp[s[r]] = r
+        #     maxLen = max(maxLen, r - l + 1)
+
+        #     r += 1
+
+        # return maxLen
