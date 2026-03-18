315e086770de2097c91b5c5fbe5aa6d83fd8fefa
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-03-18 09:28:27 +0530

    Time: 809 ms (5.01%) | Memory: 125.6 MB (5.20%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -4,42 +4,73 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
+    def reverse(self, head):
 
-    def findMid(self, head):
+        if head is None or head.next is None:
+            return head
+        newHead = self.reverse(head.next)
+        head.next.next = head
+        head.next = None
+        return newHead
+
+    def isPalindrome(self, head):
+        if head is None or head.next is None:
+            return True
         slow = head
         fast = head
-        while(fast != None and fast.next != None):
+        while(fast.next != None and fast.next.next != None):
             slow = slow.next
             fast = fast.next.next
-        return slow
-        
-    def isPalindrome(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: bool
-        """
-        if(head == None or head.next == None):
-            return True
-        
-        mid = self.findMid(head)
+        newNode = self.reverse(slow.next)
+        first = head
+        second = newNode
 
-
-        prev = None
-        curr = mid
-        while(curr):
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        
-        right = prev
-        left = head
-
-        while(right != None):
-            if (left.val != right.val):
+        while second:
+            if first.val != second.val:
+                self.reverse(newNode)
                 return False
-            left = left.next
-            right = right.next
+            else:
+                first = first.next
+                second = second.next
+        self.reverse(newNode)
         return True
+
+
+    # def findMid(self, head):
+    #     slow = head
+    #     fast = head
+    #     while(fast != None and fast.next != None):
+    #         slow = slow.next
+    #         fast = fast.next.next
+    #     return slow
+        
+    # def isPalindrome(self, head):
+    #     """
+    #     :type head: Optional[ListNode]
+    #     :rtype: bool
+    #     """
+    #     if(head == None or head.next == None):
+    #         return True
+        
+    #     mid = self.findMid(head)
+
+
+    #     prev = None
+    #     curr = mid
+    #     while(curr):
+    #         temp = curr.next
+    #         curr.next = prev
+    #         prev = curr
+    #         curr = temp
+        
+    #     right = prev
+    #     left = head
+
+    #     while(right != None):
+    #         if (left.val != right.val):
+    #             return False
+    #         left = left.next
+    #         right = right.next
+    #     return True
     
     
