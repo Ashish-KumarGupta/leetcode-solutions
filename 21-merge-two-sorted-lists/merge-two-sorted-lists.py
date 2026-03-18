3af573d7d97cae7b2d161c70cc8e39b3b584d87f
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-03-18 09:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.59%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,38 +4,50 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
+    def mergeTwoLists(self, l1, l2):
         """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        head= ListNode()
-        curr = head
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                list1 = list1.next
-                curr = curr.next
-            elif list1.val > list2.val:
-                curr.next = list2
-                list2 = list2.next
-                curr = curr.next
-            else:
-                curr.next = list1
-                list1 = list1.next
-                curr = curr.next
-                curr.next = list2
-                list2 = list2.next
-                curr= curr.next
-        while list1:
-            curr.next = list1
-            list1 = list1.next
-            curr = curr.next
-        while list2:
-            curr.next = list2
-            list2 = list2.next
-            curr = curr.next
-        return head.next
+        # head= ListNode()
+        # curr = head
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         curr.next = list1
+        #         list1 = list1.next
+        #         curr = curr.next
+        #     elif list1.val > list2.val:
+        #         curr.next = list2
+        #         list2 = list2.next
+        #         curr = curr.next
+        #     else:
+        #         curr.next = list1
+        #         list1 = list1.next
+        #         curr = curr.next
+        #         curr.next = list2
+        #         list2 = list2.next
+        #         curr= curr.next
+        # while list1:
+        #     curr.next = list1
+        #     list1 = list1.next
+        #     curr = curr.next
+        # while list2:
+        #     curr.next = list2
+        #     list2 = list2.next
+        #     curr = curr.next
+        # return head.next
 
         
+        dummy = ListNode(0)
+        curr = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        return dummy.next
