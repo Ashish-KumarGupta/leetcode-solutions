d40f8ef94071c0e55c989712982299e3563438c5
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-03-20 08:27:27 +0530

    Time: 2 ms (82.30%) | Memory: 12.5 MB (57.80%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,11 +5,15 @@ class Solution(object):
 
         for char in s:
             if char in mapp:
-                if not stack:
+                # if not stack:
+                #     return False
+                # last = stack.pop()
+                # if mapp[char] != last:
+                #     return False
+
+                if not stack or stack[-1] != mapp[char]:
                     return False
-                last = stack.pop()
-                if mapp[char] != last:
-                    return False
+                stack.pop()
             else:
                 stack.append(char)
         return len(stack) == 0
