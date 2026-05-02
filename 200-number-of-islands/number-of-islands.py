6a227073f4c444f3b236194fd0200920d7adafb4
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-05-02 15:06:13 +0530

    Time: 231 ms (48.69%) | Memory: 27.5 MB (92.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -6,22 +6,48 @@ class Solution(object):
         row = len(grid)
         col = len(grid[0])
         count = 0
-        def dfs(r, c):
-            if r < 0 or c < 0 or r >= row or c >= col or grid[r][c] == '0':
-                return
 
-            grid[r][c] = "0"
-
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
+        # DFS
 
 
+        # def dfs(r, c):
+        #     if r < 0 or c < 0 or r >= row or c >= col or grid[r][c] == '0':
+        #         return
+
+        #     grid[r][c] = "0"
+
+        #     dfs(r+1, c)
+        #     dfs(r-1, c)
+        #     dfs(r, c+1)
+        #     dfs(r, c-1)
+
+        # BFS
+
+        def bfs(r, c):
+            queue = deque()
+            queue.append((r, c))
+            grid[r][c] = '0'
+
+            while queue:
+                x, y = queue.popleft()
+
+                # 4 directions
+                directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+
+                    if (0 <= nx < row and 
+                        0 <= ny < col and 
+                        grid[nx][ny] == '1'):
+
+                        queue.append((nx, ny))
+                        grid[nx][ny] = '0'  
 
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == "1":
-                    dfs(i, j)
+                    # dfs(i, j)
+                    bfs(i, j)
                     count +=1
         return count   
