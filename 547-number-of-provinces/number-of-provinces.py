bc5046eb44ba00dbd906e850025844dea96bf649
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-05-02 12:41:46 +0530

    Time: 14 ms (28.50%) | Memory: 13.8 MB (14.29%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,27 +1,41 @@
 class Solution(object):
     def findCircleNum(self, isConnected):
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
+        
+        # visited = set()
+        # n = len(isConnected)
+        # prov = 0
+
+        # def bfs(val):
+        #     queue = deque()
+        #     queue.append(val)
+        #     while queue:
+        #         curr = queue.popleft()
+        #         if curr in visited:
+        #             continue
+        #         visited.add(curr)
+
+        #         for i in range(n):
+        #             if isConnected[curr][i] ==1 and i not in visited:
+        #                 queue.append(i)
+        # for j in range(n):
+        #     if j not in visited:
+        #         bfs(j)
+        #         prov +=1
+        # return prov
+
         visited = set()
         n = len(isConnected)
         prov = 0
 
-        def bfs(val):
-            queue = deque()
-            queue.append(val)
-            while queue:
-                curr = queue.popleft()
-                if curr in visited:
-                    continue
-                visited.add(curr)
+        def dfs(val):
+            visited.add(val)
 
-                for i in range(n):
-                    if isConnected[curr][i] ==1 and i not in visited:
-                        queue.append(i)
-        for j in range(n):
-            if j not in visited:
-                bfs(j)
+            for i in range(n):
+                if isConnected[val][i] == 1 and i not in visited:
+                    dfs(i)
+
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
                 prov +=1
         return prov
