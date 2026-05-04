413c0ab335aba7d405c55a4c7bbb86b533d78562
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-05-04 12:10:23 +0530

    Time: 4 ms (16.06%) | Memory: 12.5 MB (62.57%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,30 +1,59 @@
 class Solution(object):
 
-    def dfs(self, row, col, ans, image, color, delrow, delcol, initcolor):
+    # def dfs(self, row, col, ans, image, color, delrow, delcol, initcolor):
     
-        ans[row][col] = color  
+    #     ans[row][col] = color  
 
-        n = len(image)
-        m = len(image[0])
+    #     n = len(image)
+    #     m = len(image[0])
 
-        for i in range(4):
-            nrow = row + delrow[i]
-            ncol = col + delcol[i]
+    #     for i in range(4):
+    #         nrow = row + delrow[i]
+    #         ncol = col + delcol[i]
 
-            if (0 <= nrow < n and 0 <= ncol < m and 
-                image[nrow][ncol] == initcolor and 
-                ans[nrow][ncol] != color):
+    #         if (0 <= nrow < n and 0 <= ncol < m and 
+    #             image[nrow][ncol] == initcolor and 
+    #             ans[nrow][ncol] != color):
 
-                self.dfs(nrow, ncol, ans, image, color, delrow, delcol, initcolor)
+    #             self.dfs(nrow, ncol, ans, image, color, delrow, delcol, initcolor)
 
-    def floodFill(self, image, sr, sc, color,):
-        initcolor = image[sr][sc]
-        ans = []
-        for row in image:
-            ans.append(row[:])
+    # def floodFill(self, image, sr, sc, color,):
+    #     initcolor = image[sr][sc]
+    #     ans = []
+    #     for row in image:
+    #         ans.append(row[:])
 
-        delrow = [-1, 0, +1, 0]
-        delcol = [0, +1, 0, -1]
-        self.dfs(sr, sc, ans, image, color, delrow, delcol, initcolor)
+    #     delrow = [-1, 0, +1, 0]
+    #     delcol = [0, +1, 0, -1]
+    #     self.dfs(sr, sc, ans, image, color, delrow, delcol, initcolor)
     
-        return ans
+    #     return ans
+
+
+    # In-Place  if dont use extra memory or ans variable
+
+
+    def floodFill(self, image, sr, sc, newColor):
+        oldColor = image[sr][sc]
+        
+
+        if oldColor == newColor:
+            return image
+        
+        rows, cols = len(image), len(image[0])
+        
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return
+            if image[r][c] != oldColor:
+                return
+            
+            image[r][c] = newColor
+            
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        
+        dfs(sr, sc)
+        return image
