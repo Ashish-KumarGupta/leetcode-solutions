a2d71ea11ec8787ccb1686379cfe2e32ed934c4b
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-06-17 11:30:12 +0530

    Time: 6 ms (17.78%) | Memory: 12.8 MB (8.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,20 +1,36 @@
 class Solution(object):
     def isValid(self, s):
+        # stack = []
+        # mapp = {'}':'{', ']':'[', ')':"("}
+
+        # for char in s:
+        #     if char in mapp:
+        #         # if not stack:
+        #         #     return False
+        #         # last = stack.pop()
+        #         # if mapp[char] != last:
+        #         #     return False
+
+        #         if not stack or stack[-1] != mapp[char]:
+        #             return False
+        #         stack.pop()
+        #     else:
+        #         stack.append(char)
+        # return len(stack) == 0
+
+
+
         stack = []
-        mapp = {'}':'{', ']':'[', ')':"("}
-
-        for char in s:
-            if char in mapp:
-                # if not stack:
-                #     return False
-                # last = stack.pop()
-                # if mapp[char] != last:
-                #     return False
-
-                if not stack or stack[-1] != mapp[char]:
-                    return False
-                stack.pop()
+        for i in range(len(s)):
+            if s[i] =='(' or s[i] =='[' or s[i] == '{':
+                stack.append(s[i])
             else:
-                stack.append(char)
-        return len(stack) == 0
+                if not stack:
+                    return False
+                top = stack.pop()
+                
+                if s[i] ==')' and top!='(' or s[i] =='}' and top!='{' or s[i] ==']' and top!='[':
+                    return False
+               
+        return len(stack)==0
 
