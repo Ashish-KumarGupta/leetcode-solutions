3530207956a4013e8c5908601380137ad70c3e08
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-08-14 09:24:38 +0530

    Time: 7 ms (6.60%) | Memory: 14.8 MB (6.77%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,25 +6,42 @@ from collections import deque
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def levelOrder(self, root):
-        ans = []
+    def dfs(self, level, root, ans):
         if not root:
-            return ans
-        q = deque()
-        q.append(root)
+            return
+        if level == len(ans):
+            ans.append([])
+        ans[level].append(root.val)
 
-        while q:
-            level = []
-            for i in range(len(q)):
-                current = q.popleft()
-                level.append(current.val)
+        self.dfs(level+1, root.left, ans)
+        self.dfs(level+1, root.right, ans)
 
-                if current.left:
-                    q.append(current.left)
-                if current.right:
-                    q.append(current.right)
-            ans.append(level)
+    def levelOrder(self, root):
+
+        ans = []
+        self.dfs(0, root, ans)
         return ans
+        # ans = []
+        # if not root:
+        #     return ans
+        # q = deque()
+        # q.append(root)
+
+        # while q:
+        #     level = []
+        #     for i in range(len(q)):
+        #         current = q.popleft()
+        #         level.append(current.val)
+
+        #         if current.left:
+        #             q.append(current.left)
+        #         if current.right:
+        #             q.append(current.right)
+        #     ans.append(level)
+        # return ans
+
+
+
             
 
         
