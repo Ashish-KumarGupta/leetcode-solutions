985b23b31ee61ef5f2bf769f508acebb955e61f3
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-09-02 14:31:16 +0530

    Time: 4 ms (10.00%) | Memory: 12.3 MB (65.00%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -1,17 +1,16 @@
 class Solution(object):
     def uniformArray(self, nums1):
-        nums2 = []
-        i = nums1[0]
-        for j in range(1,len(nums1)):
-            nums2.append(i - nums1[j])
-        i+=1
-        nums2.append(nums1[-1])
+        return True
+        # nums2 = []
+        # i = nums1[0]
+        # for j in range(1,len(nums1)):
+        #     nums2.append(i - nums1[j])
+        # i+=1
+        # nums2.append(nums1[-1])
         
-        for i in range(len(nums2)):
-            if nums2[i] % 2 == 0 or nums2[i] % 2 != 0:
-                return True
-            # elif nums2[i] % 2 != 0:
-            #     return True
-            else:
-                return False
+        # for i in range(len(nums2)):
+        #     if nums2[i] % 2 == 0 or nums2[i] % 2 != 0:
+        #         return True
+        #     else:
+        #         return False
         
