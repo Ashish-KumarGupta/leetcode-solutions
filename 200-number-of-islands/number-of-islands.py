347f127c0979424d0b7349e4be066aefdc9c0090
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-09-06 18:59:04 +0530

    Time: 219 ms (75.64%) | Memory: 29.2 MB (26.29%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,53 +1,35 @@
+from collections import deque
 class Solution(object):
-    def numIslands(self, grid):
-        if not grid:
-            return 0
+    def bfs(self, row, col, visited, grid):
+        visited[row][col] = 1
+        q = deque()
+        q.append((row, col))
+        n = len(grid)
+        m = len(grid[0])
 
+        while q:
+            r,c = q.popleft()
+
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)
+]
+
+            for delrow, delcol in directions:
+                nrow = r + delrow
+                ncol = c + delcol
+                if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and grid[nrow][ncol] =='1' and visited[nrow][ncol] == 0):
+                    visited[nrow][ncol] = 1
+                    q.append((nrow, ncol))
+
+    def numIslands(self, grid):
+        iscnt = 0
         row = len(grid)
         col = len(grid[0])
-        count = 0
+        visited = [[0]* col for i in range(row)]
 
-        # DFS
-
-
-        # def dfs(r, c):
-        #     if r < 0 or c < 0 or r >= row or c >= col or grid[r][c] == '0':
-        #         return
-
-        #     grid[r][c] = "0"
-
-        #     dfs(r+1, c)
-        #     dfs(r-1, c)
-        #     dfs(r, c+1)
-        #     dfs(r, c-1)
-
-        # BFS
-
-        def bfs(r, c):
-            queue = deque()
-            queue.append((r, c))
-            grid[r][c] = '0'
-
-            while queue:
-                x, y = queue.popleft()
-
-                # 4 directions
-                directions = [(1,0), (-1,0), (0,1), (0,-1)]
-
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-
-                    if (0 <= nx < row and 
-                        0 <= ny < col and 
-                        grid[nx][ny] == '1'):
-
-                        queue.append((nx, ny))
-                        grid[nx][ny] = '0'  
 
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == "1":
-                    # dfs(i, j)
-                    bfs(i, j)
-                    count +=1
-        return count   
+                if grid[i][j] == '1' and visited[i][j] == 0:
+                    self.bfs(i, j, visited, grid)
+                    iscnt +=1
+        return iscnt
