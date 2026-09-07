18f968e359a7793b191b67281f1826ccd5f6b924
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-09-07 17:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (93.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,37 +1,33 @@
+from collections import deque
 class Solution(object):
     def orangesRotting(self, grid):
         n = len(grid)
         m = len(grid[0])
-
+        visited = [[0] * m for _ in range(n)]
         q = deque()
-        vis = 0
 
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    q.append((i, j, 0))     #row , col, time
-                elif grid[i][j] == 1:
-                    vis += 1
-
-        time = 0
-
-        drow = [-1, 0, +1, 0]
-        dcol = [0, +1, 0, -1]
+                    q.append((i,j, 0))
+                    visited[i][j] = 2
+        tm = 0 
+        directions = [(-1,0), (0,1),(1,0),(0,-1)]
 
         while q:
-            r, c, t = q.popleft()
-            time = max(time , t)
+            r,c,t = q.popleft()
+            tm = max(tm, t)
+            for dx, dy in directions:
+                newrow = r + dx
+                newcol = c + dy
+                if(newrow >= 0 and newrow < n and newcol >= 0 and newcol < m and visited[newrow][newcol] == 0 and grid[newrow][newcol] == 1):
+                    q.append((newrow, newcol, t+1))
+                    visited[newrow][newcol] = 1
 
-            for i in range(4):
-                nr = r + drow[i]
-                nc = c + dcol[i]
+        for i in range(n):
+            for j in range(m):
 
-                if (0 <= nr < n and 0 <= nc < m and grid[nr][nc] == 1):
-                    grid[nr][nc] = 2
-                    vis -= 1
-                    q.append((nr, nc, t+1))
+                if grid[i][j] == 1 and visited[i][j] == 0:
+                    return -1
 
-        if vis == 0:
-            return time
-        else:
-            return -1
+        return tm       
