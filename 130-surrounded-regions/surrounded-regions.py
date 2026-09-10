79fd17f1da68d029ae21dd1b31933f172b0291e0
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-09-10 22:43:32 +0530

    Time: 7 ms (76.21%) | Memory: 16.2 MB (83.43%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,14 +1,38 @@
+from collections import deque
 class Solution(object):
-    def dfs(self, row, col, visited, grid, directions):
+    # for dfs
+    # def dfs(self, row, col, visited, grid, directions):
+
+    #     n = len(grid)
+    #     m = len(grid[0])
+    #     visited[row][col] = 1
+        
+    #     for dx, dy in directions:
+    #         nrow = row + dx
+    #         ncol = col + dy
+    #         if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and visited[nrow][ncol] ==0 and grid[nrow][ncol] == "O"):
+    #             self.dfs(nrow, ncol, visited, grid, directions)
+
+
+
+
+
+    # for bfs
+    def bfs(self, row, col, visited, grid, directions):
         n = len(grid)
         m = len(grid[0])
+        q = deque()
+        q.append((row, col))
         visited[row][col] = 1
         
-        for dx, dy in directions:
-            nrow = row + dx
-            ncol = col + dy
-            if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and visited[nrow][ncol] ==0 and grid[nrow][ncol] == "O"):
-                self.dfs(nrow, ncol, visited, grid, directions)
+        while q:
+            r, c = q.popleft()
+            for dx, dy in directions:
+                nrow = r + dx
+                ncol = c + dy
+                if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and visited[nrow][ncol] ==0 and grid[nrow][ncol] == "O"):
+                    visited[nrow][ncol] = 1
+                    q.append((nrow, ncol))
     def solve(self, grid):
         n = len(grid)
         m = len(grid[0])
@@ -21,13 +45,13 @@ class Solution(object):
         for j in range(m):
             # for first row
             if(visited[0][j] == 0 and grid[0][j] == "O"):
-                self.dfs(0, j, visited, grid, directions)
+                self.bfs(0, j, visited, grid, directions)
                 
                 
                 
             # for last row
             if (visited[n-1][j] == 0 and grid[n-1][j] == "O"):
-                self.dfs(n-1, j, visited, grid, directions)
+                self.bfs(n-1, j, visited, grid, directions)
                 
                 
         # for col
@@ -35,11 +59,11 @@ class Solution(object):
         for i in range(n):
             # for first col
             if(visited[i][0] == 0 and grid[i][0] == "O"):
-                self.dfs(i, 0,  visited, grid, directions)
+                self.bfs(i, 0,  visited, grid, directions)
                 
             # for last col
             if(visited[i][m-1] == 0 and grid[i][m-1] == "O"):
-                self.dfs(i, m-1, visited, grid, directions)
+                self.bfs(i, m-1, visited, grid, directions)
                 
         # for all others
         for i in range(n):
