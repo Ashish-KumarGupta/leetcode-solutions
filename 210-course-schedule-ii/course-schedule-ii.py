1c5beeca65cb3c99628f6eeac0a29ced5d83e1c9
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2026-09-16 10:31:32 +0530

    Time: 7 ms (47.12%) | Memory: 14.8 MB (22.50%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def dfs(self, node, graph, visited, pathvisited, order):
+        visited[node] = 1
+        pathvisited[node] = 1
+        for neighbour in graph[node]:
+            # when the node is not visited
+            if visited[neighbour] == 0:
+                if self.dfs(neighbour, graph, visited, pathvisited, order) == True:
+                    return True
+            # if the node has been previously visited
+            # but it has to be visited on the same path
+            elif(pathvisited[neighbour]):
+                return True
+        
+        pathvisited[node] = 0
+        order.append(node)
+        return False
+    def findOrder(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+        visited = [0]*numCourses
+        pathvisited = [0]*numCourses
+        order = []
+        
+        for i in range(numCourses):
+            if visited[i] == 0:
+                if self.dfs(i, graph, visited, pathvisited, order) == True:
+                    return []
+
+        order.reverse()
+
+        return order
+        
+        
